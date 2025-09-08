3cb0d7ef9dbe839dabdbfc683030f11a78221a80
James Pritchard <jpritchard@integrated-skills.com>
2025-09-08 09:37:35 +0100

    Added CommunityToolkit

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
 namespace Bug_RatingControl
 {
@@ -9,6 +10,7 @@ namespace Bug_RatingControl
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
